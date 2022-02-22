98dadd661aa1fd263e0181948777797b8ff25df7
Scott Bailey <sbailey@anaconda.com>
2022-02-21 17:52:55 -0700

    Add more detailed testing.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,7 @@ cmake -G "Ninja" ^
       %CMAKE_ARGS% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DBUILD_TESTS=OFF ^
-      -DGHC_FILESYSTEM_BUILD_TESTING=OFF ^
+      -DGHC_FILESYSTEM_BUILD_TESTING=ON ^
       -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF ^
       %SRC_DIR%
 if errorlevel 1 exit 1
@@ -18,3 +18,25 @@ if errorlevel 1 exit 1
 :: Build and install
 ninja install
 if errorlevel 1 exit 1
+
+:: Perform tests
+test\exception
+if errorlevel 1 exit 1
+
+test\filesystem_test
+if errorlevel 1 exit 1
+
+::test\filesystem_test_cpp17
+::if errorlevel 1 exit 1
+
+::test\filesystem_test_cpp20
+::if errorlevel 1 exit 1
+
+test\fwd_impl_test
+if errorlevel 1 exit 1
+
+test\multifile_test
+if errorlevel 1 exit 1
+
+::test\std_filesystem_test
+::if errorlevel 1 exit 1
