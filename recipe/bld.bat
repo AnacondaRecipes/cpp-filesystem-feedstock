a7a9d1d758ec3cf0753b24a40c050a56c4d7ee92
Scott B <Scott@TheBaileyFamily.org>
2022-02-22 16:03:36 -0700

    Update version and fix some standards issues. (#1)
    Changes:
    
    - Update from 1.5.8 to 1.5.10.
       - It looks like the project uses odd for dev and even for release so this is a single bump of the version.
    - Add dev and doc urls.
    - Prefer ninja over make + nmake.
    - Remove "empty" `requirements:host` section.
    - Add detailed testing to build scripts.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,42 @@
-cmake -G "NMake Makefiles" -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% ^
-    -DGHC_FILESYSTEM_BUILD_TESTING=OFF -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF
+:: Windows
+
+:: Isolate the build
+mkdir build
+cd build
 if errorlevel 1 exit 1
 
-nmake
+:: Generate the build files
+cmake -G "Ninja" ^
+      %CMAKE_ARGS% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DBUILD_TESTS=OFF ^
+      -DGHC_FILESYSTEM_BUILD_TESTING=ON ^
+      -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake install
+:: Build and install
+ninja install
 if errorlevel 1 exit 1
+
+:: Perform tests
+test\exception
+if errorlevel 1 exit 1
+
+test\filesystem_test
+if errorlevel 1 exit 1
+
+::test\filesystem_test_cpp17
+::if errorlevel 1 exit 1
+
+::test\filesystem_test_cpp20
+::if errorlevel 1 exit 1
+
+test\fwd_impl_test
+if errorlevel 1 exit 1
+
+test\multifile_test
+if errorlevel 1 exit 1
+
+::test\std_filesystem_test
+::if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX ${SRC_DIR} -DCMAKE_INSTALL_LIBDIR=lib \
-    -DGHC_FILESYSTEM_BUILD_TESTING=OFF -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF
-make install
+# Isolate the build
+mkdir -p .build
+cd .build || exit 1
+
+if [[ "${target_platform}" == osx-arm64 ]]; then
+    perform_test=OFF
+else
+    perform_test=ON
+fi
+
+# Call CMake to generate the build files
+cmake -G "Ninja" \
+      ${CMAKE_ARGS} \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DGHC_FILESYSTEM_BUILD_TESTING=${perform_test} \
+      -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF \
+      ${SRC_DIR} \
+      || exit 1
+
+# Build and install
+ninja install
+
+
+# Perform tests
+if [[ "${perform_test}" == ON ]]; then
+    test/exception || exit 1
+    test/filesystem_test || exit 1
+    #test/filesystem_test_cpp17 || exit 1
+    #test/filesystem_test_cpp20 || exit 1
+    test/fwd_impl_test || exit 1
+    test/multifile_test || exit 1
+    #test/std_filesystem_test  || exit 1
+fi
